8239551725ef71ab35b60bab0bc534eb68245065
libbyberter@gmail.com <118197596+libbyberter@users.noreply.github.com>
2023-03-26 20:52:41 -0400

    saving progress 20230326 2050

--- Leaflet-part-1/static/js/logic.js
+++ Leaflet-part-1/static/js/logic.js
@@ -1,3 +1,5 @@
+
+
 // Create make map function
 function makeMap(quakeMarkersLayer) {
     console.log(`start makeMap`)
@@ -16,8 +18,8 @@ function makeMap(quakeMarkersLayer) {
     };
 
     var myMap = L.map("map", {
-        center: [40, -99],
-        zoom: 5,
+        center: [45, -111],
+        zoom: 3.5,
         layers: [mapTile, quakeMarkersLayer]
     });
 
@@ -25,6 +27,30 @@ function makeMap(quakeMarkersLayer) {
         collapsed: false
     }).addTo(myMap)
     console.log(`end makeMap`)
+    console.log(`start legend`)
+
+    // add legend (code from leaflet.js)
+    var legend = L.control({position: 'topright'});
+
+    legend.onAdd = function (map) {
+
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades = [-10, 10, 20, 30, 40, 50, 60, 70],
+            labels = [];
+
+        // loop through our density intervals and generate a label with a colored square for each interval
+        for (var l = 0; l < grades.length; l++) {
+            div.innerHTML +=
+                '<i style="background:' + depthColor(grades[l] + 1) + ';"></i> ' +
+                grades[l] + (grades[l + 1] ? '&ndash;' + grades[l + 1] + '<br>' : '+');
+            // console.log(depthColor(grades[l] + 1))
+            }
+            
+        return div;
+
+    };
+
+    legend.addTo(myMap);
 }
 
 
@@ -36,20 +62,66 @@ function createMarkers(response) {
 
     // list to hold coordinates of each quake
     var quakeMarkers = []
+    // var quakeDepth = []
+    // var magnitude = []
 
     // loop through file to get all quake coordinates
     for (var i = 0; i < quakes.length; i++) {
         var quake = quakes[i].geometry
-        var quakeLocation = L.marker([quake.coordinates[1],quake.coordinates[0]])
-        quakeMarkers.push(quakeLocation)
+        var quakeLocation = [quake.coordinates[1],quake.coordinates[0]]
+        // quakeMarkers.push(quakeLocation)
+
+        var depth = quake.coordinates[2]
+        // quakeDepth.push(depth)
+
+
+
+        
+        var mag = quakes[i].properties.mag
+        if (mag < 0) {
+            var magnitude = 0}
+        else 
+            var magnitude = mag
+        // quakeMagnitude.push(magnitude)
+        // console.log(depth)
+        // console.log(magnitude)
+
+        quakeMarkers.push(
+            L.circle(quakeLocation, {
+                stroke:true,
+                fillOpacity: 0.85,
+                color: 'gray',
+                fillColor: depthColor(depth),
+                radius: markerSize(magnitude),
+                weight: 1
+            })
+        )
     }
+
     console.log(`end makeMap`)
     // make layer group and pass info to make map function
     makeMap(L.layerGroup(quakeMarkers))
 
 }
 
+function depthColor(depth) {
+    return  depth > 70 ? '#6e016b':
+            depth > 60 ? '#88419d':
+            depth > 50 ? '#8c6bb1':
+            depth > 40 ? '#8c96c6':
+            depth > 30 ? '#9ebcda':
+            depth > 20 ? '#bfd3e6':
+            depth > 10 ? '#e0ecf4':
+            depth > -10 ? '#f7fcfd':
+            'white';
 
+// ['#edf8fb','#bfd3e6','#9ebcda','#8c96c6','#8856a7','#810f7c']
+}
+
+
+function markerSize(magnitude) {
+    return Math.sqrt(magnitude) * 50000;
+  }
 
 
 
